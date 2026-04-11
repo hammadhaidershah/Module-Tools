18f64d1876060b73af6ec9b96a5254d65cfe44c3
Hammad Haider <hammadhaider@Hammads-MacBook-Pro.local>
2026-04-11 12:13:29 +0200

    given the ls command to list directories

--- individual-shell-tools/ls/script-01.sh
+++ individual-shell-tools/ls/script-01.sh
@@ -14,35 +14,4 @@ fi
 # TODO: Write a command to list the files and folders in this directory.
 # The output should be a list of names including child-directory, script-01.sh, script-02.sh, and more.
 
-hammadhaider@Hammads-MacBook-Pro individual-shell-tools % ls -R
-awk             grep            ls              sed
-cat             helper-files    README.md       wc
-
-./awk:
-README.md               script-02.sh            script-05.sh
-scores-table.txt        script-03.sh            script-06-stretch.sh
-script-01.sh            script-04.sh            script-07-stretch.sh
-
-./cat:
-script-01.sh            script-03.sh
-script-02.sh            script-04-stretch.sh
-
-./grep:
-dialogue-2.txt  dialogue.txt    script-02.sh    script-04.sh    script-06.sh
-dialogue-3.txt  script-01.sh    script-03.sh    script-05.sh    script-07.sh
-
-./helper-files:
-helper-1.txt    helper-2.txt    helper-3.txt
-
-./ls:
-child-directory script-01.sh    script-02.sh    script-03.sh    script-04.sh
-
-./ls/child-directory:
-helper-1.txt    helper-2.txt    helper-3.txt
-
-./sed:
-input.txt       script-02.sh    script-04.sh    script-06.sh
-script-01.sh    script-03.sh    script-05.sh
-
-./wc:
-script-01.sh    script-02.sh    script-03.sh
+ls 
