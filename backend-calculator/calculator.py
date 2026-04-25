10543b24f7390055ef5c958cb8b43068a9b4d360
hammadhaidershah <hammad.haider.shah@gmail.com>
2026-04-25 11:56:33 +0200

    Backend canlulator

---
+++ backend-calculator/calculator.py
@@ -0,0 +1,10 @@
+def calculate(num1, operation, num2):
+    if operation == "plus":
+        return num1 + num2
+    elif operation == "minus":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        return num1 / num2
+    
